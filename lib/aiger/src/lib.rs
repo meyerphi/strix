0f8c28bc7b268111147534875427f1195d9bb8e7
Philipp Meyer <meyerphi@users.noreply.github.com>
2021-03-20 15:29:40 +0100

    Documentation for aiger crate

--- lib/aiger/src/lib.rs
+++ lib/aiger/src/lib.rs
@@ -1,3 +1,6 @@
+//! Low-level bindings to the aiger library and a high-level aiger constructor.
+
+#[doc(hidden)]
 mod bindings;
 
 use std::collections::hash_map::Entry;
@@ -9,28 +12,39 @@ use std::os::raw::{c_char, c_int, c_uint, c_void};
 
 use bindings::*;
 
-// low level aiger interface
-
+/// The raw pointer type for an aiger.
 pub type AigerRaw = aiger;
 
+/// The false literal.
+pub const AIGER_FALSE: c_uint = aiger_false;
+/// The true literal.
+pub const AIGER_TRUE: c_uint = aiger_true;
+
+/// Returns the sign of a literal, i.e. whether it is complemented or not.
 pub const fn aiger_sign(lit: c_uint) -> c_uint {
     lit & 1
 }
+/// Strips the sign of a literal, i.e. returns the uncomplemented version.
 pub const fn aiger_strip(lit: c_uint) -> c_uint {
     lit & !1
 }
+/// Inverts a literal, i.e. toggles the complement state.
 pub const fn aiger_not(lit: c_uint) -> c_uint {
     lit ^ 1
 }
+/// Returns the uncomplemented literal associated to a variable.
 pub const fn aiger_var2lit(var: c_uint) -> c_uint {
     var << 1
 }
+/// Returns the variable associated to a literal.
 pub const fn aiger_lit2var(lit: c_uint) -> c_uint {
     lit >> 1
 }
 
+/// An and-inverter graph (aiger) circuit.
 #[derive(Debug)]
 pub struct Aiger {
+    /// The underlying raw pointer for the C interface.
     aiger: *mut AigerRaw,
 }
 
@@ -40,65 +54,86 @@ impl Drop for Aiger {
     }
 }
 
+/// The mode for writing the aiger circuit.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum AigerMode {
+    /// Option to write the circuit in ASCII format.
     Ascii,
+    /// Option to write the circuit in compressed binary format.
     Binary,
 }
 
 impl AigerMode {
-    fn as_aiger_mode(&self) -> aiger_mode {
+    /// Converts the aiger mode to the enum option in the C interface.
+    fn as_aiger_mode(self) -> aiger_mode {
         match self {
-            AigerMode::Ascii => aiger_mode_aiger_ascii_mode,
-            AigerMode::Binary => aiger_mode_aiger_binary_mode,
+            Self::Ascii => aiger_mode_aiger_ascii_mode,
+            Self::Binary => aiger_mode_aiger_binary_mode,
         }
     }
 }
 
 impl Aiger {
-    pub fn new() -> Result<Aiger, &'static str> {
+    /// Returns a new aiger circuit.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the aiger circuit could not be initialized.
+    pub fn new() -> Result<Self, &'static str> {
         let aiger = unsafe { aiger_init() };
         if aiger.is_null() {
             Err("Failed to initialize aiger")
         } else {
-            Ok(Aiger { aiger })
+            Ok(Self { aiger })
         }
     }
 
+    /// Construct a wrapped aiger circuit from a raw pointer.
+    ///
     /// # Safety
     ///
-    /// aiger must point to a valid aiger struct.
-    pub unsafe fn from_raw(aiger: *mut AigerRaw) -> Aiger {
-        Aiger { aiger }
+    /// The pointer `aiger` must point to a valid aiger struct.
+    pub unsafe fn from_raw(aiger: *mut AigerRaw) -> Self {
+        Self { aiger }
     }
 
+    /// Extract the raw pointer in this wrapped aiger circuit.
+    ///
     /// # Safety
     ///
-    /// self must not be dropped while the pointer is used
+    /// This struct must not be dropped while the pointer is used.
     pub unsafe fn raw_ptr(&self) -> *mut AigerRaw {
         self.aiger
     }
 
+    /// The maximum variable index.
+    /// The maximum literal value is then `2*maxvar+1`.
     pub fn maxvar(&self) -> c_uint {
         unsafe { (*self.aiger).maxvar }
     }
 
+    /// The number of inputs.
     pub fn num_inputs(&self) -> c_uint {
         unsafe { (*self.aiger).num_inputs }
     }
 
+    /// The number of latches.
     pub fn num_latches(&self) -> c_uint {
         unsafe { (*self.aiger).num_latches }
     }
 
+    /// The number of outputs.
     pub fn num_outputs(&self) -> c_uint {
         unsafe { (*self.aiger).num_outputs }
     }
 
+    /// The number of and gates.
     pub fn num_ands(&self) -> c_uint {
         unsafe { (*self.aiger).num_ands }
     }
 
+    /// Adds an input to the aiger circuit with the given literal,
+    /// which must be uncomplemented, and an optional name.
     pub fn add_input(&mut self, lit: c_uint, name: Option<&str>) {
         match name {
             Some(name) => {
@@ -111,6 +146,8 @@ impl Aiger {
         }
     }
 
+    /// Adds an output to the aiger circuit with the given literal as next value
+    /// and an optional name.
     pub fn add_output(&mut self, lit: c_uint, name: Option<&str>) {
         match name {
             Some(name) => {
@@ -123,10 +160,14 @@ impl Aiger {
         }
     }
 
+    /// Adds an and gate to the aiger circuit with given `lhs` literal,
+    /// which must not be complemented, and the given right-hand-sides as inputs.
     pub fn add_and(&mut self, lhs: c_uint, rhs0: c_uint, rhs1: c_uint) {
         unsafe { aiger_add_and(self.aiger, lhs, rhs0, rhs1) };
     }
 
+    /// Add a latch to the aiger circuit with the given literal,
+    /// which must be uncomplemented, the given next literal and an optional name.
     pub fn add_latch(&mut self, lit: c_uint, next: c_uint, name: Option<&str>) {
         match name {
             Some(name) => {
@@ -139,17 +180,30 @@ impl Aiger {
         }
     }
 
+    /// Sets the reset value of the latch with literal `lit` to `reset`.
+    /// The value `reset` must be either constant true, constant false
+    /// or equal to `lit`.
     pub fn add_reset(&mut self, lit: c_uint, reset: c_uint) {
         unsafe { aiger_add_reset(self.aiger, lit, reset) };
     }
 
+    /// Writes the aiger circuit to the given writer in the given mode.
+    ///
+    /// # Errors
+    ///
+    /// If the writer returns an error during the write, then  this error is returned.
+    /// If a write operation does not write any bytes, then an I/O error of kind [`WriteZero`] is returned.
+    ///
+    /// [`WriteZero`]: std::io::ErrorKind::WriteZero
     pub fn write<W: Write>(&self, writer: W, mode: AigerMode) -> io::Result<()> {
-        // wrapped writer to recover errors
+        /// Wrapped writer to recover errors.
         struct WrappedWriter<W> {
+            /// The wrapped writer.
             writer: W,
+            /// The last error returned by the writer, if any.
             error: Option<io::Error>,
         };
-        // rust version of aiger_put
+        /// Rust version of aiger_put.
         extern "C" fn aiger_put<W>(character: c_char, data: *mut c_void) -> c_int
         where
             W: Write,
@@ -201,15 +255,26 @@ impl Aiger {
         }
     }
 
-    pub fn read<R: Read>(reader: R) -> io::Result<Aiger> {
-        let aiger = Aiger::new().map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
-
-        // wrapped reader to recover errors
+    /// Reads an aiger circuit from the given reader.
+    ///
+    /// # Errors
+    ///
+    /// If the reader returns an error during the read, then this error is returned.
+    /// If the aiger circuit is malformed, then an I/O error of kind [`InvalidData`] with the
+    /// underlying error message is returned.
+    /// If the circuit creation fails, an I/O error of kind [`Other`] is returned.
+    ///
+    /// [`InvalidData`]: std::io::ErrorKind::InvalidData
+    /// [`Other`]: std::io::ErrorKind::Other
+    pub fn read<R: Read>(reader: R) -> io::Result<Self> {
+        /// Wrapped reader to recover errors.
         struct WrappedReader<R> {
+            /// The wrapped reader.
             reader: R,
+            /// The last error returned by the reader, if any.
             error: Option<io::Error>,
         };
-        // rust version of aiger_get
+        /// Rust version of aiger_get.
         extern "C" fn aiger_get<R>(data: *mut c_void) -> c_int
         where
             R: Read,
@@ -230,6 +295,8 @@ impl Aiger {
                 }
             }
         }
+        // initialize aiger
+        let aiger = Self::new().map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
         // place reader on the heap
         let data = Box::into_raw(Box::new(WrappedReader {
             reader,
@@ -257,53 +324,104 @@ impl Aiger {
     }
 }
 
-// high level aiger interface and builder
-
+/// Wrapped literal for safe use with [`AigerConstructor`].
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct Literal(c_uint);
 
 impl Literal {
-    pub const FALSE: Literal = Literal(aiger_false);
-    pub const TRUE: Literal = Literal(aiger_true);
+    /// The constant false literal.
+    pub const FALSE: Self = Self(AIGER_FALSE);
+    /// The constant true literal.
+    pub const TRUE: Self = Self(AIGER_TRUE);
 
-    pub fn from_bool(val: bool) -> Literal {
+    /// Returns the constant literal with given boolean value.
+    pub fn from_bool(val: bool) -> Self {
         if val {
-            Literal::TRUE
+            Self::TRUE
         } else {
-            Literal::FALSE
+            Self::FALSE
         }
     }
 }
 
 impl std::ops::Not for Literal {
-    type Output = Literal;
+    type Output = Self;
 
     fn not(self) -> Self::Output {
-        Literal(aiger_not(self.0))
+        Self(aiger_not(self.0))
     }
 }
 
+/// Literal pair to use in a cache. Should always be normalized such that
+/// the first literal is less or equal than the second.
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 struct LiteralPair {
+    /// First element of the pair.
     lit0: Literal,
+    /// Second element of the pair.
     lit1: Literal,
 }
 
+/// A high-level constructor for an aiger circuit that can be used to
+/// safely and incrementally add elements.
+///
+/// The constructor ensures that in the aiger circuit all
+/// inputs appear before latches and all latches before and gates.
+/// During construction, it hashes the inputs of existing and gates
+/// and reuses them, as well as performing simplications for the inputs.
+/// This ensures that an equivalent circuit is produced, but which may be smaller
+/// than a direct construction.
+///
+/// To use the constructor, literals have to be constructed incrementally and
+/// bound to variables, except for the constant true and false literals
+/// which are available through [`Literal::TRUE`] and [`Literal::FALSE`].
+///
+/// # Examples
+///
+/// The following example constructs a simple circuit for storing the input value
+/// `val` on an update by `upd` in the latch `latch`, with the output `cur`
+/// for the current value of the latch:
+///
+/// ```
+/// # use aiger::{AigerConstructor, Literal};
+/// let mut constructor = AigerConstructor::new(2, 1).unwrap();
+/// let upd = constructor.add_input("upd");
+/// let val = constructor.add_input("val");
+/// let latch = constructor.add_latch("latch");
+/// let latch_next = constructor.add_ite(upd, val, latch);
+/// constructor.set_latch_next(latch, latch_next);
+/// constructor.set_latch_reset(latch, Literal::TRUE);
+/// constructor.add_output("cur", latch);
+/// let aiger = constructor.into_aiger();
+/// ```
 pub struct AigerConstructor {
+    /// The aiger circuit that is currently being constructed.
     aig: Aiger,
+    /// The count of the number of inputs that have already been added.
     cur_input: usize,
+    /// The number of inputs that were specified.
     num_inputs: usize,
+    /// The count of the number of latches that have already been added.
     cur_latch: usize,
+    /// The number of latches that were specified.
     num_latches: usize,
+    /// The names of latches that were already added.
     latches: Vec<String>,
+    /// The count of and gates that were added.
     cur_and: usize,
+    /// The cache of already added and gates, mapping their inputs to the uncomplemented literal.
     and_cache: HashMap<LiteralPair, Literal>,
 }
 
 impl AigerConstructor {
-    pub fn new(num_inputs: usize, num_latches: usize) -> Result<AigerConstructor, String> {
+    /// Creates a new aiger circuit constructor with a pre-specified number of inputs and latches.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the initialization of the aiger circuit fails.
+    pub fn new(num_inputs: usize, num_latches: usize) -> Result<Self, String> {
         let aig = Aiger::new()?;
-        Ok(AigerConstructor {
+        Ok(Self {
             aig,
             cur_input: 0,
             num_inputs,
@@ -315,6 +433,11 @@ impl AigerConstructor {
         })
     }
 
+    /// Adds an and gate to the circuit with `lhs` and `rhs` as inputs,
+    /// and returns the literal for the and gate.
+    ///
+    /// May return a constant literal or an already existing literal
+    /// if a simplification rule is used or the literal is in the cache.
     pub fn add_and(&mut self, lhs: Literal, rhs: Literal) -> Literal {
         if lhs == rhs {
             lhs
@@ -346,10 +469,20 @@ impl AigerConstructor {
         }
     }
 
+    /// Adds an or gate to the circuit with `lhs` and `rhs` as inputs,
+    /// and returns the literal for the or gate.
+    ///
+    /// This function simply forwards to [`add_and`](AigerConstructor::add_and) and uses the same
+    /// simplification and cache rules.
     pub fn add_or(&mut self, lhs: Literal, rhs: Literal) -> Literal {
         !self.add_and(!lhs, !rhs)
     }
 
+    /// Adds gates for an "if-then-else" construct, and returns the literal for the output.
+    ///
+    /// This function performs additional simplifations over a direct construction with
+    /// and gates, but falls back to such construction otherwise. In that case, it
+    /// adds three and gates where the output corresponds to `(¬lit ∨ then_lit) ∧ (lit ∨ else_lit)`.
     pub fn add_ite(&mut self, lit: Literal, then_lit: Literal, else_lit: Literal) -> Literal {
         if lit == Literal::TRUE || then_lit == else_lit {
             then_lit
@@ -370,6 +503,11 @@ impl AigerConstructor {
         }
     }
 
+    /// Adds an input with the given name to the circuit, and returns the literal for the input.
+    ///
+    /// # Panics
+    ///
+    /// Panics if already a number of inputs as specified during the initialization were added.
     pub fn add_input(&mut self, name: &str) -> Literal {
         assert!(self.cur_input < self.num_inputs);
         let lit = Literal(aiger_var2lit((1 + self.cur_input) as c_uint));
@@ -378,6 +516,11 @@ impl AigerConstructor {
         lit
     }
 
+    /// Adds a latch with the given name to the circuit, and returns the literal for the latch.
+    ///
+    /// # Panics
+    ///
+    /// Panics if already a number of latches as specified during the initialization were added.
     pub fn add_latch(&mut self, name: &str) -> Literal {
         assert!(self.cur_latch < self.num_latches);
         self.latches.push(name.to_string());
@@ -388,10 +531,16 @@ impl AigerConstructor {
         lit
     }
 
+    /// Adds an output wtih the given name and the next value given by `lit` to the circuit.
     pub fn add_output(&mut self, name: &str, lit: Literal) {
         self.aig.add_output(lit.0, Some(name));
     }
 
+    /// Returns the index of a latch literal in the internal list of names.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the given literal is not a latch.
     fn latch_index(&self, latch: Literal) -> usize {
         let var = aiger_lit2var(latch.0) as usize;
         if !(var > self.num_inputs && var <= 1 + self.num_inputs + self.cur_latch) {
@@ -400,22 +549,38 @@ impl AigerConstructor {
         var - (1 + self.num_inputs)
     }
 
+    /// Sets the next value of a latch `next`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the given literal is not a latch.
     pub fn set_latch_next(&mut self, latch: Literal, next: Literal) {
         let index = self.latch_index(latch);
         let name = &self.latches[index];
         self.aig.add_latch(latch.0, next.0, Some(name));
     }
 
+    /// Sets the next reset value of a latch to `reset`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the given literal is not a latch or the reset value is not constant true or false.
     pub fn set_latch_reset(&mut self, latch: Literal, reset: Literal) {
         assert!(reset == Literal::TRUE || reset == Literal::FALSE);
+        let _ = self.latch_index(latch);
         self.aig.add_reset(latch.0, reset.0);
     }
 
-    pub fn write<W: Write>(&self, writer: W, mode: AigerMode) -> io::Result<()> {
-        self.aig.write(writer, mode)
-    }
-
+    /// Consumes this constructor and returns the aiger circuit constructed by it.
+    ///
+    /// # Panics
+    ///
+    /// Panics if not all latches and inputs as initially specified were added,
+    /// or not all latches have been assigned a next value.
     pub fn into_aiger(self) -> Aiger {
+        assert!(self.cur_input == self.num_inputs);
+        assert!(self.cur_latch == self.num_latches);
+        assert!(self.latches.len() == self.num_latches);
         self.aig
     }
 }
@@ -565,13 +730,13 @@ mod tests {
     #[test]
     fn test_aiger_read() {
         // valid read
-        let buf = "aag 0 0 0 0 0\n".as_bytes();
-        let result = Aiger::read(buf);
+        let aig_valid = "aag 0 0 0 0 0\n".as_bytes();
+        let result = Aiger::read(aig_valid);
         assert!(result.is_ok());
 
         // read succeeds, but invalid data
-        let buf = "not an aiger\n".as_bytes();
-        let result = Aiger::read(buf);
+        let aig_invalid = "not an aiger\n".as_bytes();
+        let result = Aiger::read(aig_invalid);
         assert!(result.is_err());
         assert_eq!(result.unwrap_err().kind(), io::ErrorKind::InvalidData);
 
@@ -585,16 +750,17 @@ mod tests {
     #[test]
     fn test_aiger_write() {
         // valid write
-        let mut aig = AigerConstructor::new(2, 1).unwrap();
-        let upd = aig.add_input("upd");
-        let val = aig.add_input("val");
-        let latch = aig.add_latch("latch");
+        let mut constructor = AigerConstructor::new(2, 1).unwrap();
+        let upd = constructor.add_input("upd");
+        let val = constructor.add_input("val");
+        let latch = constructor.add_latch("latch");
 
-        let latch_next = aig.add_ite(upd, val, latch);
-        aig.set_latch_next(latch, latch_next);
-        aig.set_latch_reset(latch, Literal::TRUE);
-        aig.add_output("cur", latch);
+        let latch_next = constructor.add_ite(upd, val, latch);
+        constructor.set_latch_next(latch, latch_next);
+        constructor.set_latch_reset(latch, Literal::TRUE);
+        constructor.add_output("cur", latch);
 
+        let aig = constructor.into_aiger();
         let mut aig_vec = Vec::new();
         let result = aig.write(&mut aig_vec, AigerMode::Ascii);
         assert!(result.is_ok());
